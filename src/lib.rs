aefa80a3b931ea289d2531af0ce8115c74968dcc
llalma <lhulsmanbenson@gmail.com>
2023-07-11 22:57:38 +1000

    Move dataframe into liv for WASM
    added test for simplifying complex values. I.E Function consitings of cell references

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,170 @@
+extern crate console_error_panic_hook;
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+use strum_macros::EnumString;
+use std::str::FromStr;
+use std::collections::HashMap;
+use core::fmt;
+use ndarray::AssignElem;
+
 mod value;
+mod coordinate;
 mod functions;
-mod dataframe;
+
+
+use ndarray::prelude::*;
+use wasm_bindgen::describe::FUNCTION;
+use crate::value::{solve_function, Value};
+use crate::coordinate::Coordinate;
+use crate::functions::concat::concat;
+use crate::functions::add::add;
+
+
+#[derive(Clone)]
+#[derive(Debug)]
+#[derive(PartialEq)]
+#[derive(EnumString)]
+pub enum FUNCTION{
+    ADD,
+    MOD,
+    CONCAT
+}
+impl FUNCTION{
+    fn starts_with(value: &str) -> Result<FUNCTION, &str>{
+        /* Attempts to convert any given string to a Function ENUM, tries to split on first "("
+        then parse that into a enum */
+
+        //Get String before first occurance of '('
+        let mut splitter = value.splitn(2, '(');
+        let first = splitter.next().unwrap().trim();
+
+        //Parse into Enum
+        if let Ok(res) = FUNCTION::from_str(first){
+            return Ok(res)
+        }
+
+        return Err("Error, not a valid Function")
+    }
+}
+impl fmt::Display for FUNCTION {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+
+#[wasm_bindgen]
+#[derive(Debug)]
+pub struct DataFrame{
+    data: Array2<Value>,
+    references: HashMap<Coordinate, Vec<Coordinate>>
+}
+
+impl DataFrame {
+    pub fn new(data: Vec<Vec<String>>) -> Self {
+        //Try parse each value in array2 to their corresponding value enum.
+        //Trys to assume from string, need a way to parse inputs as a certain type.
+
+        let mut data_transformed = Array2::<Value>::default((data.len(), data[0].len()));
+        for (i, mut row) in data_transformed.axis_iter_mut(Axis(0)).enumerate() {
+            for (j, col) in row.iter_mut().enumerate() {
+                *col = Value::create_from_str(data[i][j].to_string());
+            }
+        }
+
+        Self {
+            data: data_transformed,
+            references: HashMap::new()
+        }
+    }
+
+    fn simplify(&self, coords1: Coordinate, coords2: Coordinate) -> Array2<Value> {
+        /*
+        For the given coordinate ranges simplify any functions or Cell references to simple Values.
+        Simple values  in this case being anything but cell references and function
+         */
+
+        // Get the slice of data we care about
+        let mut binding = self.data.clone();
+        let mut printing_slice = binding.slice_mut(s!(coords1.row..coords2.row+1, coords1.column..coords2.column+1));
+
+        //Solve all the values in the slice to non complex data
+
+        return Array2::from_shape_vec( printing_slice.dim(),
+                                    printing_slice.into_iter()
+                                    .map(|v| self.solve_cell(v))
+                                    .collect::<Vec<Value>>()
+            ).unwrap();
+    }
+
+    fn solve_cell(&self, cell: &Value) -> Value {
+        /*
+        Recursive function which solves cell references and functions until a simple datatype is returned.
+        In this context a simple datatype is any value that is not a cell reference or function
+         */
+
+        //This clones the values sot ehy can be inserted into DF if required without de-referencing
+        return match cell {
+            Value::CELL_REFERENCE(_) => self.solve_cell(cell.solve_reference(&self.data).unwrap()).clone(),
+            Value::FUNCTION(function, values) => solve_function(function, &(values.into_iter().map(|v| self.solve_cell(v)).collect::<Vec<Value>>())).unwrap(),
+            _ => cell.clone()
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use crate::value::Value;
+    use ndarray::prelude::*;
+    use crate::{DataFrame, FUNCTION};
+    use crate::coordinate::Coordinate;
+
+    #[test]
+    fn test_df_print() {
+        assert!(false)
+    }
+
+    #[test]
+    fn test_simplify(){
+
+        /*
+        Create the input values
+         */
+        let mut input = DataFrame::new(
+          vec![
+            vec!["3".to_string()],   //A1
+            vec!["ADD([A0],[A0])".to_string()], //A2
+            vec!["ADD([A1],[A0])".to_string()]  //A3
+          ]
+        );
+
+        // //Add the cell references to the struct for the input
+        // input.references.insert(
+        //     Coordinate {row:0 ,column:0},
+        //     vec![Coordinate {row:1,column:0}]
+        // );
+        // input.references.insert(
+        //     Coordinate {row:0 ,column:0},
+        //     vec![Coordinate {row:2,column:0}]
+        // );
+        // input.references.insert(
+        //     Coordinate {row:1 ,column:0},
+        //     vec![Coordinate {row:2,column:0}]
+        // );
+
+        /*
+        Create the expected result
+         */
+        let expected = DataFrame {
+            data: arr2(&[[Value::I32(3)],   //A1
+                        [Value::I32(6)],    //A2
+                        [Value::I32(9)]]),   //A3
+            references: HashMap::new()
+        };
+
+        assert_eq!(expected.data, input.simplify(Coordinate{row:0,column:0}, Coordinate{row:2,column:0}));
+    }
+}
+
--- src/value.rs
+++ src/value.rs
@@ -1,15 +1,25 @@
 use std::fmt;
-use chrono::{DateTime, Utc};
+use regex::Regex;
+use chrono::{NaiveDateTime,};
+use ndarray::Array2;
+use qndr::{get_alphabets, get_numbers};
+use crate::coordinate::Coordinate;
+use crate::FUNCTION;
+use crate::functions::add::add;
 
+#[allow(clippy::upper_case_acronyms)]
+#[allow(non_camel_case_types)]
 #[derive(PartialEq)]
 #[derive(Debug)]
 #[derive(Clone)]
-pub(crate) enum Value{
+pub enum Value{
     NULL(),
     I32(i32),
     F32(f32, usize),
     STRING(String),
-    DATETIME(DateTime<Utc>, String)
+    DATETIME(NaiveDateTime, String),
+    FUNCTION(FUNCTION, Vec<Value>),
+    CELL_REFERENCE(Coordinate)
 }
 
 //Implementation for default Values of each datatype. Mainly used for comparisons between Enum variants
@@ -18,7 +28,110 @@ impl Value{
     fn i32_default() -> Self {Value::I32(0)}
     fn f32_default() -> Self {Value::F32(0.0, 0)}
     fn string_default() -> Self {Value::STRING("".to_owned())}
-    fn datetime_default() -> Self {Value::DATETIME(DateTime::<Utc>::MIN_UTC, "%Y%m%d".to_owned())}
+    fn function_default() -> Self{Value::FUNCTION(FUNCTION::ADD, vec![Value::I32(1), Value::I32(1)])}
+    fn reference_default() -> Self{Value::CELL_REFERENCE(Coordinate{row:0,column:0})}
+
+    pub fn create_from_str(value: String) -> Value {
+
+        //If input is ""
+        if value == "".to_owned(){
+            return Value::NULL()
+        }
+
+        //If input is datetime
+        //Currently only parses a single format
+        const DATETIME_FORMAT: &str = "%Y%m%d %H%M%S";
+        if let Ok(res) = NaiveDateTime::parse_from_str(&value, DATETIME_FORMAT){
+            return Value::DATETIME(res, DATETIME_FORMAT.to_owned());
+        }
+
+        //If input is i32
+        if let Ok(res) = value.parse::<i32>() {
+            return Value::I32(res)
+        }
+
+        //If input is f32
+        if let Ok(res) = value.parse::<f32>() {
+            let re = Regex::new(r"\.(\d*)").unwrap();
+            let precision = re.find(&value).map(|x| x.len()-1).unwrap_or(1);   //Minus 1 since capture includes the decimal point
+            return Value::F32(res, precision)
+        }
+
+        //Check if is a Function string - String can represent other functions as this solves for recursive methods
+        if let Ok(res) = FUNCTION::starts_with(&value){
+
+            //Split on the opening (
+            let mut splitter_opening = value.splitn(2, '(');
+            let mut values = splitter_opening.nth(1).unwrap().trim();
+
+            //Split on the closing )
+            let mut splitter_closing = values.splitn(2, ')');
+            values = splitter_closing.next().unwrap().trim();
+
+            // Split on all , not inside (). This is done manually as rust regex does not support negative lookahead
+            let mut brackets_count = 0;
+            let mut end_last_string_index = 0;
+            let mut split_values: Vec<Value> = vec![];
+            for (i, c) in values.chars().enumerate(){
+
+                //Add a count if a bracket was opened
+                if c == '(' {brackets_count+=1}
+                //Minus a count if a bracket was closed
+                if c == ')' {brackets_count-=1}
+
+                //Add any values to vec if can split as not inside ()
+                if c == ',' && brackets_count == 0{
+                    split_values.push(Value::create_from_str(values[end_last_string_index..i].to_owned()));
+                    end_last_string_index = i+1 //Increment by 1 so next split does not contain the comma just split on
+                }
+
+                //If the last char then add anything else to the vec
+                //Can do this as the string should never end in a comma so should not duplicate by accident
+                if i == values.len()-1{
+                    split_values.push(Value::create_from_str(values[end_last_string_index..].to_owned()));
+                }
+            }
+
+            return Value::FUNCTION(res, split_values)
+        }
+
+        //If input is a cell reference
+        let re = Regex::new(r"^\[{1}\w*\]{1}$").unwrap();
+        if let Some(res) = re.find(&value.to_uppercase()) {
+
+            //Find the letters to convert to column number
+            let column: i32 = get_alphabets(res.as_str()).chars()
+                .rev()
+                .enumerate()
+                .map(|(i,c)| i32::pow(26,i as u32)*(c as i32 - 64))
+                .sum::<i32>()-1; //Minus 1 as 0 indexing
+
+            //Find the digits to convert to row number - can unwrap as we only got numeric chars
+            let row = get_numbers(res.as_str()).parse::<i32>().unwrap();
+
+            return Value::CELL_REFERENCE(Coordinate{row:row as usize, column:column as usize})
+        }
+
+        //If all checks are done assume its an actual string
+        return Value::STRING(value)
+    }
+
+    pub fn solve_reference<'a>(&'a self, data: &'a Array2<Value>) -> Option<&Value>{
+        /*
+        Returns the value in a given cell for the referenced cell.
+         */
+
+        //Dont need the if, but cant find how to resolve without if let or match statement
+        if let Self::CELL_REFERENCE(coord) = self {
+            return data.get((coord.row, coord.column))
+        }
+
+        return None
+    }
+}
+
+impl Default for Value {
+    fn default() -> Self {Value::NULL()}
 }
 
 //Formatting for Value Enum
@@ -29,10 +142,31 @@ impl fmt::Display for Value {
             Value::I32(val) => write!(f, "{}", val.to_string()),
             Value::F32(val, precision) => write!(f, "{}", format!("{val:.prec$}", val=val, prec=precision)),
             Value::STRING(val) => write!(f, "{}", val),
-            Value::DATETIME(val, format) => write!(f, "{}", val.format(format))
+            Value::DATETIME(val, format) => write!(f, "{}", val.format(format)),
+            Value::FUNCTION(function, vals) => write!(f, "{}", "Not Implemented"),
+            Value::CELL_REFERENCE(coords) => write!(f, "{}", coords)
         }
     }
 }
+
+pub fn solve_function(
+    function: &FUNCTION,
+    values: &Vec<Value>) -> Result<Value, String> {
+    /*
+    Implements the matching logic for determining which function to execute dependent on the given ENUM
+     */
+
+
+    //I feel like I should be borrowing values here instead of cloning but then have to deal with lifetimes in return component
+    return match function {
+        FUNCTION::ADD => add(values[0].clone(), values[1].clone()),
+
+        _ => Ok(Value::I32(1))
+    }
+}
+
+
+
 
 // Function to transform the input values into the same type, using the type with the most precision as the target datatype
 pub(crate) fn get_same_form(input_1: Value, input_2: Value) -> Result<(Value, Value), String>{
@@ -82,7 +216,44 @@ fn transform(input: Value, target: Value) -> Result<Value, String>{
 
 #[cfg(test)]
 mod tests {
+    use chrono::{NaiveDateTime};
+    use crate::coordinate::Coordinate;
+    use crate::FUNCTION;
     use crate::value::{get_same_form, Value};
+
+    #[test]
+    fn test_from_string_null(){
+        let input = Value::create_from_str("".to_string());
+        let expected = Value::NULL();
+        assert_eq!(input, expected)
+    }
+
+    #[test]
+    fn test_from_string_i32(){
+        let input = Value::create_from_str("3".to_string());
+        let expected = Value::I32(3);
+        assert_eq!(input, expected)
+    }
+
+    #[test]
+    fn test_from_string_datetime(){
+        let input = Value::create_from_str("20210418 210328".to_string());
+        let expected = Value::DATETIME(NaiveDateTime::parse_from_str("20210418 210328", "%Y%m%d %H%M%S").unwrap(), "%Y%m%d %H%M%S".to_owned());
+        assert_eq!(input, expected)
+    }
+
+    #[test]
+    fn test_from_string_f32(){
+        let mut input = Value::create_from_str("3.4".to_string());
+        let mut expected = Value::F32(3.4,1);
+        assert_eq!(input, expected);
+        input = Value::create_from_str("5.143".to_string());
+        expected = Value::F32(5.143,3);
+        assert_eq!(input, expected);
+        input = Value::create_from_str("5.".to_string());
+        expected = Value::F32(5.0,0);
+        assert_eq!(input, expected);
+    }
 
     #[test]
     fn test_f32_and_i32() {
@@ -122,7 +293,7 @@ mod tests {
     #[test]
     fn test_datetime_formatting(){
         // Input values
-        let current_datetime = chrono::Utc::now();
+        let current_datetime = chrono::Utc::now().naive_utc();
         let input_format = "%Y %m %d %H:%M";
 
         //Create Value datatype and Expected outcome
@@ -131,5 +302,29 @@ mod tests {
 
         //Check outputs match
         assert_eq!(expected.to_string(), input.to_string())
+    }
+
+    #[test]
+    fn test_from_string_string(){
+        let input = Value::create_from_str("this is a test string".to_owned());
+        let expected = Value::STRING("this is a test string".to_owned());
+        assert_eq!(input, expected)
+    }
+
+    #[test]
+    fn test_from_string_cell_reference(){
+        let input = Value::create_from_str("[ZA62]".to_owned());
+        let expected = Value::CELL_REFERENCE(Coordinate {row:62, column:677});
+        assert_eq!(input, expected)
+    }
+
+    #[test]
+    fn test_from_string_function(){
+        let input = Value::create_from_str("ADD(2,ADD(1,2))".to_owned());
+        let expected = Value::FUNCTION(FUNCTION::ADD,
+                                       vec![Value::I32(2), Value::FUNCTION(FUNCTION::ADD,
+                                                            vec![Value::I32(1), Value::I32(2)])]);
+
+        assert_eq!(input, expected)
     }
 }
